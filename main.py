ba2387d6e5e85c2dc31eb67f4f7e474fab29e280
Xavier MD <37403873+XMousset@users.noreply.github.com>
2026-02-05 12:27:10 +0100

    clean up code formatting and improve readability

--- main.py
+++ main.py
@@ -6,20 +6,21 @@ from tkinter import filedialog, messagebox
 
 UNLOCK = True
 
+
 def select_files():
     """Opens a file dialog for the user to select multiple PDF files to merge.
     If at least two files are selected, converts those file paths to Path
     objects and calls merge_pdf with the Path list.
     """
     pdf_files = filedialog.askopenfilenames(
-        title= "PDF unlocker - from files",
-        filetypes= [("PDF files", "*.pdf")],
-        initialdir= Path(__file__).parent
+        title="PDF unlocker - from files",
+        filetypes=[("PDF files", "*.pdf")],
+        initialdir=Path(__file__).parent,
     )
-    
+
     if not pdf_files:
         return None
-    
+
     pdf_paths = [Path(f) for f in pdf_files]
     pdf_paths.sort()
     unlock_pdf(pdf_paths)
@@ -31,50 +32,43 @@ def select_folder():
     paths to Path objects and calls merge_pdf with the Path list.
     """
     folder = filedialog.askdirectory(
-        title= "PDF unlocker - from folder",
-        initialdir= Path(__file__).parent,
+        title="PDF unlocker - from folder",
+        initialdir=Path(__file__).parent,
     )
     if not folder:
         return None
-    
-    pdf_names = [f
-        for f in os.listdir(folder)
-        if f.lower().endswith(".pdf")
-    ]
-    
+
+    pdf_names = [f for f in os.listdir(folder) if f.lower().endswith(".pdf")]
+
     if not pdf_names:
         messagebox.showerror("Error", "No PDF in selected folder.")
         return None
-    
+
     pdf_paths = [Path(folder) / f for f in pdf_names]
     pdf_paths.sort()
     unlock_pdf(pdf_paths)
 
 
-def unlock_pdf(pdf_paths : list[Path]):
+def unlock_pdf(pdf_paths: list[Path]):
     """Merges multiple PDF files into a single PDF file. Prompts the user to
     select the output pdf path via a save file dialog.
     """
     root = tk.Tk()
     root.withdraw()
-    
+
     print(f"Unlocking PDFs...")
-    
+
     for pdf in pdf_paths:
-        
+
         if UNLOCK:
             process = f"Unblock-File -Path '{pdf}'"
         else:
             process = f"Set-Content -Path '{pdf}:Zone.Identifier' -Value '[ZoneTransfer]`nZoneId=3'"
-        
-        subprocess.run([
-            "powershell",
-            "-Command",
-            process
-            ])
-        
+
+        subprocess.run(["powershell", "-Command", process])
+
         print(f"{pdf.name} done.")
-    
+
     print(f"Unlocked done for {len(pdf_paths)} PDFs.")
 
 
@@ -92,22 +86,20 @@ def main_tk():
     pos_y = int(screen_height / 4 - window_height / 2)
     root.geometry(f"{window_width}x{window_height}+{pos_x}+{pos_y}")
     root.resizable(False, False)
-    
+
     nb_cols = 5
     nb_rows = 4
-    
+
     label = tk.Label(
-        root,
-        text= "Choose a merging option:",
-        font= ("Calibri", 15)
+        root, text="Choose a merging option:", font=("Calibri", 15)
     )
-    label.grid(row= 0, column= 0, columnspan= nb_cols)
+    label.grid(row=0, column=0, columnspan=nb_cols)
 
     btn_files = tk.Button(
         root,
         text="Select Files",
-        command= select_files,
-        font= ("Calibri", 15, "bold")
+        command=select_files,
+        font=("Calibri", 15, "bold"),
     )
     btn_files.grid(row=2, column=1)
 
@@ -115,16 +107,16 @@ def main_tk():
         root,
         text="Select Folder",
         command=select_folder,
-        font= ("Calibri", 15, "bold")
+        font=("Calibri", 15, "bold"),
     )
     btn_folder.grid(row=2, column=3)
 
     for i in range(nb_rows):
-        root.grid_rowconfigure(i, weight= 1)
-    
+        root.grid_rowconfigure(i, weight=1)
+
     for i in range(nb_cols):
-        root.grid_columnconfigure(i, weight= 1)
-    
+        root.grid_columnconfigure(i, weight=1)
+
     root.mainloop()
 
 
